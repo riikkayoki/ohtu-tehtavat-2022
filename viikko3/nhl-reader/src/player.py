a65d9cb21ea18855c2b50ebdbc6fed26423cd4c8
riikkayoki <74421508+riikkayoki@users.noreply.github.com>
2022-03-22 14:12:07 +0200

    2. Siistimpi pelaajalista

--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -6,4 +6,8 @@ class Player:
         self.assists = assists
 
     def __str__(self):
-        return f'{self.name} team {self.team} goals {self.goals} assists {self.assists}'
+        return f'{self.name:20} {self.team:5} {self.goals:1} + {self.assists:1} = {self.__eq__()}'
+
+    def __eq__(self):
+        return self.goals + self.assists
+
