e5a3c5bbedaf851bb16de27a3a7f8e4ce05c68e3
riikkayoki <74421508+riikkayoki@users.noreply.github.com>
2022-03-24 19:11:13 +0200

    2. Yhden tuotteen lisäämisen jälkeen ostoskorissa on 1 tavara.

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -17,8 +17,7 @@ class Ostoskori:
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
-        # lisää tuotteen
-        pass
+        self._ostoskori.append(lisattava)
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
