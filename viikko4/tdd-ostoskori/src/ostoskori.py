c0bd91db681acbc640a3e8a1464d29c8643646aa
riikkayoki <74421508+riikkayoki@users.noreply.github.com>
2022-03-24 20:17:45 +0200

    14. Jos koriin on lisätty tuote ja sama tuote poistetaan, on kori tämän jälkeen tyhjä

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -6,37 +6,36 @@ class Ostoskori:
         self._ostoskori = {}
         self._hinta = 0
         self._maara = 0
-        self.ostoslista = []
+        self._ostoslista = []
 
     def tavaroita_korissa(self):
         return self._maara
 
     def hinta(self):
         return self._hinta
-        # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
         if lisattava.nimi() not in self._ostoskori:
             self._ostoskori[lisattava.nimi()] = Ostos(lisattava)
         else:
             self._ostoskori[lisattava.nimi()].muuta_lukumaaraa(1)
+
         self._hinta += lisattava.hinta()
         self._maara += 1
-
 
     def poista_tuote(self, poistettava: Tuote):
         if poistettava.nimi():
             self._ostoskori[poistettava.nimi()].muuta_lukumaaraa(-1)
+            if self._ostoskori[poistettava.nimi()].lukumaara() == 0:
+                del self._ostoskori[poistettava.nimi()]
+
         self._hinta -= poistettava.hinta()
         self._maara -= 1
 
-
     def tyhjenna(self):
-        pass
-        # tyhjentää ostoskorin
+        self.ostoskori = {}
 
     def ostokset(self):
-
         for ostos in self._ostoskori.values():
-            self.ostoslista.append(ostos)
-        return self.ostoslista
+            self._ostoslista.append(ostos)
+        return self._ostoslista
--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -86,7 +86,7 @@ class TestOstoskori(unittest.TestCase):
         ostos = self.kori.ostokset()[0]
         self.assertEqual(ostos.tuotteen_nimi(), "Maito")
         self.assertEqual(ostos.lukumaara(), 2)
-    
+
     def test_jos_korissa_on_kaksi_samaa_tuotetta_ja_toinen_naista_poistetaan_jaa_koriin_ostos_jossa_on_tuotetta_yksi_kpl(self):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
@@ -96,4 +96,12 @@ class TestOstoskori(unittest.TestCase):
         self.assertEqual(ostos.lukumaara(), 2)
         self.kori.poista_tuote(maito)
         self.assertEqual(ostos.lukumaara(), 1)
+
+    def test_jos_koriin_on_lisatty_tuote_ja_sama_tuote_poistetaan_on_kori_taman_jalkeen_tyhja(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.poista_tuote(maito)
+        ostokset = self.kori.ostokset()
+
+        self.assertEqual(len(ostokset), 0)
 
